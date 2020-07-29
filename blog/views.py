46d7f4c19f716901caadeb168d375b2032116f12
ddszorro <dds@thewink.com>
2020-07-29 16:04:05 +0100

    added email share and comments

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .models import Post
+from django.core.mail import send_mail
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -33,6 +35,40 @@ def post_detail(request, year, month, day, post):
                                 publish__year=year,
                                 publish__month=month,
                                 publish__day=day)
+
+    # list of active comments
+    comments = post.comments.filter(active=True) 
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
     return render(request,
                 'blog/post/detail.html',
-                {'post': post})
+                {'post': post,
+                'comments': comments,
+                'new_comment': new_comment,
+                'comment_form': comment_form})
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    form = EmailPostForm()
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'admin@thewink.com', [cd['to']])
+            sent = True
+
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
